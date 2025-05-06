bd95b50a2d703a9452a19adb31ed4e27395c4d4c
Heeko <60233099+HeekoOfficial@users.noreply.github.com>
2025-05-06 15:22:48 +0200

    Update support_node.tf

--- support_node.tf
+++ support_node.tf
@@ -1,30 +1,3 @@
-
-resource "macaddress" "k3s-support" {}
-
-locals {
-  support_node_settings = merge(
-    {
-      cores         = 2
-      sockets       = 1
-      memory        = 4096
-      storage_type  = "scsi"
-      storage_id    = "local-lvm"
-      disk_size     = "10G"
-      user          = "support"
-      network_tag   = -1
-      db_name       = "k3s"
-      db_user       = "k3s"
-      network_bridge = "vmbr0"
-    },
-    var.support_node_settings
-  )
-support_node_ip = cidrhost(var.control_plane_subnet, 0)
-}
-
-locals {
-  lan_subnet_cidr_bitnum = split("/", var.lan_subnet)[1]
-}
-
 resource "proxmox_vm_qemu" "k3s-support" {
   target_node = var.proxmox_node
   name        = join("-", [var.cluster_name, "support"])
@@ -33,17 +6,17 @@ resource "proxmox_vm_qemu" "k3s-support" {
 
   pool = var.proxmox_resource_pool
 
-  # cores = 2
   cores   = local.support_node_settings.cores
   sockets = local.support_node_settings.sockets
   memory  = local.support_node_settings.memory
 
-
   agent = 1
+
+  # Schijfconfiguratie
   disk {
-    type    = local.support_node_settings.storage_type
-    storage = local.support_node_settings.storage_id
-    size    = local.support_node_settings.disk_size
+    type    = local.support_node_settings.storage_type  # 'scsi', 'ide', 'sata', etc.
+    storage = local.support_node_settings.storage_id    # ID van de opslagpool (bijv. 'local-lvm')
+    size    = local.support_node_settings.disk_size      # Grootte van de schijf (bijv. '10G')
   }
 
   network {
@@ -72,8 +45,6 @@ resource "proxmox_vm_qemu" "k3s-support" {
 
   ipconfig0 = "ip=${local.support_node_ip}/${local.lan_subnet_cidr_bitnum},gw=${var.network_gateway}"
 
-  #sshkeys = file(var.authorized_keys_file)
-
   nameserver = var.nameserver
 
   connection {
@@ -86,12 +57,10 @@ resource "proxmox_vm_qemu" "k3s-support" {
     destination = "/tmp/install.sh"
     content = templatefile("${path.module}/scripts/install-support-apps.sh.tftpl", {
       root_password = random_password.support-db-password.result
-
       k3s_database = local.support_node_settings.db_name
       k3s_user     = local.support_node_settings.db_user
       k3s_password = random_password.k3s-master-db-password.result
-      
-      http_proxy  = var.http_proxy
+      http_proxy   = var.http_proxy
     })
   }
 
@@ -100,55 +69,6 @@ resource "proxmox_vm_qemu" "k3s-support" {
       "chmod u+x /tmp/install.sh",
       "/tmp/install.sh",
       "rm -r /tmp/install.sh",
-    ]
-  }
-}
-
-resource "random_password" "support-db-password" {
-  length           = 16
-  special          = false
-  override_special = "_%@"
-}
-
-resource "random_password" "k3s-master-db-password" {
-  length           = 16
-  special          = false
-  override_special = "_%@"
-}
-
-resource "null_resource" "k3s_nginx_config" {
-
-  depends_on = [
-    proxmox_vm_qemu.k3s-support
-  ]
-
-  triggers = {
-    config_change = filemd5("${path.module}/config/nginx.conf.tftpl")
-  }
-
-  connection {
-    type = "ssh"
-    user = local.support_node_settings.user
-    host = local.support_node_ip
-  }
-
-  provisioner "file" {
-    destination = "/tmp/nginx.conf"
-    content = templatefile("${path.module}/config/nginx.conf.tftpl", {
-      k3s_server_hosts = [for ip in local.master_node_ips :
-        "${ip}:6443"
-      ]
-      k3s_nodes = concat(local.master_node_ips, [
-        for node in local.listed_worker_nodes :
-        node.ip
-      ])
-    })
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf",
-      "sudo systemctl restart nginx.service",
     ]
   }
 }
