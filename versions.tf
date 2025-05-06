bec3bdf14f3ced27f517a949b98751811b9c88d8
Heeko <60233099+HeekoOfficial@users.noreply.github.com>
2025-05-06 14:54:11 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,15 +2,11 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.38.1" # Ensure compatibility with Heeko module
+      version = "~> 0.38.1"
     }
-  }
-}
 
     macaddress = {
       source  = "ivoronin/macaddress"
       version = "0.3.0"
     }
   }
-}
-
